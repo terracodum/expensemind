61142375d32359557772b3e84907bc9a787bfeb7
terracodum <nikitakharlov007@gmail.com>
2026-04-22 21:56:58 +0600

    refactor(parser): replace NewParser func with Factory struct and Create method

--- backend/internal/parser/factory.go
+++ backend/internal/parser/factory.go
@@ -4,7 +4,9 @@ import (
 	"github.com/terracodum/expensemind/backend/internal/errors"
 )
 
-func NewParser(contentType string) (Parser, error) {
+type Factory struct{}
+
+func (f Factory) Create(contentType string) (Parser, error) {
 	switch contentType {
 	case "text/csv":
 		return &CSVParser{}, nil
